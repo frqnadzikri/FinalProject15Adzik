7369209532b0b7d675ba4cab85d37e038d0b62c9
“radennobioyetgit config --global user.name “radennobioyet <radensnpmb@gmail.com>
2025-09-28 21:26:09 +0700

    raden merubah

--- FinalProject15.java
+++ FinalProject15.java
@@ -2,5 +2,6 @@ public class FinalProject15 {
 
     public static void main(String[] args) {
         System.out.println("Hi masht raden,aku adzik");
+        System.out.println("halo masht adzik, aku raden");
     }
 }
